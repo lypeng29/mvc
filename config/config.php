dc75fe6aa54dab3a153c81f2c84f4d934d582642
lypeng <893371810@qq.com>
2018-07-02 10:33:34 +0800

    测试忽略data里面的内容，但保留data文件夹

--- config/config.php
+++ config/config.php
@@ -24,7 +24,7 @@ return array(
     'DATA_CACHE_COMPRESS'   =>  false,   // 数据缓存是否压缩缓存
     // 'DATA_CACHE_CHECK'      =>  false,   // 数据缓存是否校验缓存
     'DATA_CACHE_PREFIX'     =>  '',     // 缓存前缀
-    'DATA_CACHE_TYPE'       =>  'Memcache',  // 数据缓存类型,支持:File|Db|Memcache|Memcached|Redis
+    'DATA_CACHE_TYPE'       =>  'File',  // 数据缓存类型,支持:File|Db|Memcache|Memcached|Redis
     'DATA_CACHE_PATH'       =>  DIR_ROOT . '/data/cache',// 缓存路径设置 (仅对File方式缓存有效)
 	'DATA_CACHE_TABLE'		=>	'sys_cache', // 缓存数据库表（针对Db缓存）
 	'DATA_CACHE_TIMEOUT'	=>	false,
